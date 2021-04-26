0c637aba09490db6c59b9a2e3628ab786551f757
Cristian <cristian.sepulveda.co@usach.cl>
2021-04-25 23:30:47 -0400

    react-create-app on Heroku

--- app/models/ThesisJavierUtilities.rb
+++ app/models/ThesisJavierUtilities.rb
@@ -1,7 +1,7 @@
 class ThesisJavierUtilities
     #include HTTParty
    
-    
+    heroku run rails console
     def selectAllThesis
         url = 'http://ec2-100-25-103-59.compute-1.amazonaws.com/tracker/thesis/'
         response = RestClient.get url, {Authorization: 'Token b99aa300d382f7491e0e6103d8cf5cd55aeecc3e'}
